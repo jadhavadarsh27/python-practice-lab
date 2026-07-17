d86a16b0f828399285cb80f97f72d60aafede9b9
Adarsh <jadhavadarsh27@gmail.com>
2026-07-17 23:59:33 +0530

    Add List Program

---
+++ Data-Structures/List.py
@@ -0,0 +1,11 @@
+fruits = ["Apple", "Banana", "Mango"]
+
+fruits.extend(["Dragon", "Chikuu"])
+
+print(fruits)
+
+
+
+student = ["adarsh", 1, "jadhavadarsh27@gmail.com", 8080477503]
+
+print(student)
