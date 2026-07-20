6bf9a2658ab5d51ab082bf1d6f4b25185e9b5571
Adarsh <jadhavadarsh27@gmail.com>
2026-07-20 23:00:21 +0530

    Add API GET request example

---
+++ API/Get.py
@@ -0,0 +1,9 @@
+import requests
+
+response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+
+else:
+    print("API failed....")
