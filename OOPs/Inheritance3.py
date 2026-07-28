ffea17e90952c0f39efabc6fb84c1775f703d52d
Adarsh <jadhavadarsh27@gmail.com>
2026-07-28 16:40:37 +0530

    Practice.

---
+++ OOPs/Inheritance3.py
@@ -0,0 +1,13 @@
+class Animal:
+    def sound(self):
+        print("Animal makes sound....")
+
+class Dog(Animal):
+    def bark(self):
+        print("Barking...")
+
+d = Dog()
+
+d.sound()
+d.bark()
+
