aff87a42525400ced0b655098bbc7c6becdf9748
Adarsh <jadhavadarsh27@gmail.com>
2026-07-31 22:40:26 +0530

    Practice

---
+++ Number-Programs/prime_number3.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("Its not")
+else:
+
+    for i in range(2, num):
+        if num % i == 0:
+            print("Its not")
+            break
+    else:
+        print("Its Prime")
