e9b7e3a74721c336166dcb377054f4948dbf2261
Adarsh <jadhavadarsh27@gmail.com>
2026-08-06 20:58:48 +0530

    Practiice

---
+++ OOPs/Polymorphism4.py
@@ -0,0 +1,17 @@
+class Animal:
+    def sound(self):
+        print("Animal makes sound")
+
+class Dog(Animal):
+    def sound(self):
+        print("Bark")
+
+class Cat(Animal):
+    def sound(self):
+        print("Meow")
+
+d = Dog()
+c = Cat()
+
+d.sound()
+c.sound()
