a5d878495eb5e39f9b41ca4b3b49be29c97275b5
Adarsh <jadhavadarsh27@gmail.com>
2026-08-09 19:57:59 +0530

    Add Constructor code

---
+++ OOPs/Constructor.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print("Name :", self.name)
+        print("Age :", self.age)
+
+student = Student("Adarsh", 23)
+
+student.display()
