2474521cffb277a872c48eb11c5eaac0a99cf44b
Adarsh <jadhavadarsh27@gmail.com>
2026-08-10 19:05:43 +0530

    Add math module examples

---
+++ Libraries/math_module.py
@@ -0,0 +1,8 @@
+import math
+
+number = int(input("Enter a number: "))
+
+print("Square root: ", math.sqrt(number))
+print("Power: ", math.pow(2, 3))
+print("Factorial: ", math.factorial(number))
+print("Pi: ", math.pi)
