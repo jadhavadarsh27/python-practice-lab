21aa268910b02cfc5c8dee6ffca9995905eff6c1
Adarsh <jadhavadarsh27@gmail.com>
2026-08-11 19:30:47 +0530

    Practice

--- Libraries/math_module.py
+++ Libraries/math_module.py
@@ -6,3 +6,4 @@ print("Square root: ", math.sqrt(number))
 print("Power: ", math.pow(2, 3))
 print("Factorial: ", math.factorial(number))
 print("Pi: ", math.pi)
+    
---
+++ Libraries/math_module_2.py
@@ -0,0 +1,6 @@
+import math
+
+number = int(input("Enter a number: "))
+
+print("The factorial of", number, "is: ", math.factorial(number))
+print("The Power of: ", math.pow(2, 3))
