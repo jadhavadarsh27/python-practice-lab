3066728a44872b0305f376c1dd8086acbe36278c
Adarsh <jadhavadarsh27@gmail.com>
2026-08-11 20:25:37 +0530

    Add basic_plot example

---
+++ Libraries/External/Matplotlib/basic_plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5, 6]
+y = [2, 4, 6, 6, 10, 12]
+
+plt.plot(x, y)
+
+plt.xlabel("X values")
+plt.ylabel("Y values")
+plt.title("Sample graph")
+
+plt.show()
