32fef7a092f5c66ebde9dd21562fc9e93d6600f3
Adarsh <jadhavadarsh27@gmail.com>
2026-08-13 16:20:31 +0530

    Practice

---
+++ Libraries/External/Matplotlib/basic_plot-2.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+x = [0, 1, 2, 3, 4]
+y = [2, 4, 4, 8, 10]
+
+plt.plot(x, y)
+
+plt.xlabel("X values")
+plt.ylabel("Y values")
+plt.title("Information")
+
+plt.show()
