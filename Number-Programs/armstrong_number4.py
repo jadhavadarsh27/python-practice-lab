998028ac9e834cc3b95ea7c48fd1f36ce2e3e07c
Adarsh <jadhavadarsh27@gmail.com>
2026-08-15 20:41:17 +0530

    Add Armstrong Number code

---
+++ Number-Programs/armstrong_number4.py
@@ -0,0 +1,15 @@
+num = int(input("Enter a number: "))
+
+temp = num
+sum = 0
+
+while(num > 0):
+    digit = num % 10
+    sum = sum + digit ** 3
+    num = num // 10
+
+if sum == temp:
+    print("Num is Arm")
+
+else:
+    print("not")
