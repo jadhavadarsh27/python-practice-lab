85bc2a318e8665d9a209880a0f12d6ce818baafa
Adarsh <jadhavadarsh27@gmail.com>
2026-08-26 18:59:13 +0530

    Practice

---
+++ OOPs/Super-3.py
@@ -0,0 +1,11 @@
+class EkveeraHeights:
+    def Awing(self):
+        print("Flat number A11")
+
+class Roommates(EkveeraHeights):
+    def core(self):
+        super().Awing()
+        print("Adarsh, Pritesh, Rohit, Pavan, Shivam, Pranav, Sahil")
+
+r = Roommates()
+r.core()
