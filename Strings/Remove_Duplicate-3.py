6c1a8eaa901467d8497577abbc52af2512c67259
Adarsh <jadhavadarsh27@gmail.com>
2026-08-29 21:11:04 +0530

    Practice

--- Strings/Remove_Duplicate-3.py
+++ Strings/Remove_Duplicate-3.py
@@ -1,8 +1,11 @@
-text = input("Enter a text: ")
+t = input("Enter a text: ")
+
+text = t.lower()
 
 result = " "
 
 for ch in text:
     if ch not in result:
         result = result + ch
+
 print(result)
