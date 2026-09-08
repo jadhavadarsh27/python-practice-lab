af70770ebb049d6052eedb6b4cbd3fb4258bbde6
Adarsh <jadhavadarsh27@gmail.com>
2026-09-08 22:19:53 +0530

    Practice

---
+++ Number-Programs/armstrong_number5.py
@@ -0,0 +1,16 @@
+num = int(input("Enter a number: "))
+
+temp = num
+sum = 0
+
+while (num > 0):
+    digit = num % 10
+    sum = sum + digit ** 3
+    num = num // 10
+
+if temp == sum:
+    print("Its arm")
+else:
+    print("Its not")
+
+    
