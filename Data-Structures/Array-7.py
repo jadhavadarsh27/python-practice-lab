bae560002847a6dccaa5939bd4d8a9b68402c10d
Adarsh <jadhavadarsh27@gmail.com>
2026-09-24 19:01:30 +0530

    Practice

--- Data-Structures/Array-7.py
+++ Data-Structures/Array-7.py
@@ -3,3 +3,4 @@ from array import array
 arr = array("i",[1, 2, 3, 4])
 
 print(arr)
+ 
---
+++ Data-Structures/Dict-7.py
@@ -0,0 +1,9 @@
+dict1 = {
+    "Name" : "Adarsh",
+    "Age" : 24,
+    "Role" : "DevOps"
+    }
+
+dict1["salary"]=555555
+
+print(dict1)
